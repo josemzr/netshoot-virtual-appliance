57a30cefbe37424869e5a8cfcca1e5d611db4d49
Jose Manzaneque Ramos <vaulner@gmail.com>
2022-10-18 13:30:19 +0200

    Update debian-settings.sh

--- scripts/debian-settings.sh
+++ scripts/debian-settings.sh
@@ -35,29 +35,7 @@ sed -i s/debian/netshoot/g /etc/hosts
 
 sed -i 's/#Banner none/Banner \/etc\/issue.net/g' /etc/ssh/sshd_config
 
-echo '> Enable rc.local facility for debian-init.py'
-cat << EOF > /etc/rc.local
-#!/bin/sh -e
-#
-# rc.local
-#
-# This script is executed at the end of each multiuser runlevel.
-# Make sure that the script will "exit 0" on success or any other
-# value on error.
-#
-# In order to enable or disable this script just change the execution
-# bits.
-#
-# By default this script does nothing.
-
-if [ ! -f /etc/debian.config ]; then
-    /sbin/debian-init.py
-    echo "\$(date)" > /etc/debian.config
-fi
-
-exit 0
-EOF
-chmod +x /etc/rc.local
-systemctl daemon-reload
+echo '> Run debian-setup script at boot'
+echo "@reboot root  if [ ! -f /etc/ran_customization ]; then /opt/debian-setup.sh &> /var/log/debian-customization.log; fi" >> /etc/crontab
 
 echo '> Done'
